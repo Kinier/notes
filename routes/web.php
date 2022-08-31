ed62a719a342bc9940567199ed58eaa91b8daa1c
kinier <govormaxsim@gmail.com>
2022-08-31 17:30:04 +0300

    new routes add

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use App\Router\Web;
+use App\Middleware\AuthMiddleware;
 
 $router = new Web();
 
-$router->get('/', \App\Controllers\NoteController::class, 'index');
+
+$router->get('/',
+             \App\Controllers\NoteController::class,
+             'index',
+             ['middlewareFunction' => 'App\Middleware\AuthMiddleware::isUser', 'arg' => 'register']);
 $router->get('/nopage', \App\Controllers\ErrorController::class, 'nopage');
+$router->get('/register', \App\Controllers\UserController::class, 'register');
+$router->get('/login', \App\Controllers\UserController::class, 'login');
+
+$router->post('/auth/register', \App\Controllers\Auth\AuthController::class, 'register');
 
 $router->done();
