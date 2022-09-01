e4dadc64d3179390d70f0a55fc839bcb937ad058
kinier <govormaxsim@gmail.com>
2022-09-01 18:01:40 +0300

    noteCreateForm add

--- static/js/form.js
+++ static/js/form.js
@@ -62,3 +62,38 @@ if (registerForm !== null) {
         }
     })
 }
+
+
+let noteCreateForm = document.querySelector('#note-create');
+if (noteCreateForm !== null) {
+    noteCreateForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
+
+        let errors = {}
+        let inputs = Array.from(e.target)
+        inputs.forEach((input) => {
+            if (input.type != 'submit') {
+                if (input.value == '') {
+                    errors[input.id] = input.value;
+                }
+            }
+        })
+        if (Object.keys(errors).length !== 0) {
+            for (const errorKey in errors) {
+                // todo add error message for every input that have error
+            }
+        }
+
+        console.log(inputs)
+        let response = await (await fetch('/note/new/create', {
+            method: "POST",
+            body: JSON.stringify({[inputs[0].name]: inputs[0].value, [inputs[1].name]: inputs[1].value})
+        })).json()
+        console.log(response)
+        if (response.status === 'ok') {
+            window.location.replace('/')
+        } else {
+            // todo add errors
+        }
+    })
+}
