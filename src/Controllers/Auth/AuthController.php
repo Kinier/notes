23d227f0cc9929fb496a736dfd9a436e33646d62
kinier <govormaxsim@gmail.com>
2022-09-01 16:48:48 +0300

    auth controller add

---
+++ src/Controllers/Auth/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace App\Controllers\Auth;
+
+use App\Config\Database;
+use App\Helpers\Response;
+
+class AuthController
+{
+    public static function register()
+    {
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $db = new Database();
+        $connection = $db->connect();
+        $statementObject = $connection->prepare(
+            "INSERT INTO `user`(`password`, `email`) VALUES (?,?)"
+        );
+        $result = $statementObject->execute([$password, $email]);
+
+        if ($result){
+            $_SESSION['user']['email'] = $email;
+            $_SESSION['user']['password'] = $password;
+            Response::jsonOK();
+        }else{
+//            Response::page('login',['errors'=>['server'=>'user with such parameters already exist']]);
+            Response::jsonError();
+        }
+
+    }
+
+    public static function login()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+
+
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $db = new Database();
+        $connection = $db->connect();
+        $statementObject = $connection->prepare(
+            "SELECT * FROM `user` WHERE email=? AND password=?"
+        );
+        $statementObject->execute([$email, $password]);
+        $user = $statementObject->fetch();
+        if ($user){
+            $_SESSION['user']['email'] = $user['email'];
+            $_SESSION['user']['password'] = $user['password'];
+            Response::jsonOK();
+        }else{
+            Response::jsonError();
+        }
+
+    }
+
+
+    public static function logout()
+    {
+        session_destroy();
+        header("Location: /login");
+        die();
+    }
+}
