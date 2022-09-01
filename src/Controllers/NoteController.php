8ee2bed2ef0d0a7f51682c1bec31c3d1db532240
kinier <govormaxsim@gmail.com>
2022-09-01 21:18:35 +0300

    note preview page add

--- src/Controllers/NoteController.php
+++ src/Controllers/NoteController.php
@@ -11,8 +11,8 @@ class NoteController{
     public static function create(){
         $data = json_decode(file_get_contents('php://input'), true);
 
-        $title = $data['title'];
-        $text = $data['text'];
+        $title = nl2br(($data['title']));
+        $text = nl2br(($data['text']));
         $userId = $_SESSION['user']['id'];
 
         $db = new Database();
@@ -41,6 +41,20 @@ class NoteController{
     public static function createPage()
     {
         Response::page('note');
+    }
+
+    public static function notePreviewPage()
+    {
+        $noteId = $_GET['id'];
+        $userId = $_SESSION['user']['id'];
+
+        $db = new Database();
+        $connection = $db->connect();
+        $statementObject = $connection->prepare("SELECT * FROM note WHERE id = ? and user_id = ?"); // FIXME WHERE user_id = `$_SESSION[userId]`
+        $statementObject->execute([$noteId, $userId]);
+        $note = $statementObject->fetch();
+
+        Response::page("preview", ['note'=>$note]);
     }
 
 }
