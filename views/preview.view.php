6783467947ec3740090406e8844d1d3a8a879fd9
kinier <govormaxsim@gmail.com>
2022-09-01 21:19:06 +0300

    preview view page add

---
+++ views/preview.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once __DIR__ . '/components/head.php'; ?>
+<body>
+
+<div class="main">
+    <header>
+        <div><a href="/note/new">Create note</a></div>
+        <div id="exit-account" ><a href="/auth/logout">exit account</a> </div>
+    </header>
+
+    <div class="note-preview">
+        <?php
+            if (isset($data)){?>
+                <div class="note-preview__title"><?= $data['note']['title'] ?></div>
+                <div class="note-preview__text"><?= $data['note']['text'] ?></div>
+            <?php }
+        ?>
+    </div>
+
+</div>
+<script src="/static/js/form.js" type="module"></script>
+
+<?php
+include_once __DIR__ . '/components/footer.php'; ?>
+</body>
+</html>
